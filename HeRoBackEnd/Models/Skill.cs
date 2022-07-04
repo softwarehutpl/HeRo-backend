f5e3c593b5bf52d377f73dc02f596bdf4aa1a5f7
Mateusz Jeżewski <mateusz.jezewski@softwarehut.com>
2022-07-04 15:16:41 +0200

    test

---
+++ HeRoBackEnd/Models/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeRoBackEnd.Models
+{
+    public class Role
+    {
+        [Key]
+        [Required(ErrorMessage = "Field is required!")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Field is required!")]
+        [MaxLength(75, ErrorMessage = "Name of field is to long (max. 75 characters!")]
+        public string Name { get; set; }
+
+        //public virtual ICollection<CampaignRequirement> CampaignRequirements {get;set;}
+    }
+}
