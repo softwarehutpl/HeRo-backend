e302b40fa082698e413974f28d6e542731291d2f
Kprzyby <kprzy@vp.pl>
2022-07-04 14:53:09 +0200

    fixed namespace on Recruitment class

---
+++ Services/Services/RecruitmentServices.cs
@@ -0,0 +1,24 @@
+﻿using HeRoBackEnd.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services.Services
+{
+    internal class RecruitmentServices
+    {
+        private readonly DataContext context;
+        public RecruitmentServices(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Recruitment> GetRecruitments()
+        {
+            return null;
+        }
+    }
+}
