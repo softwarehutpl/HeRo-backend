bdf19b7fe25e79624a7b7c980d7c0b09fea508db
Kprzyby <kprzy@vp.pl>
2022-07-12 11:34:03 +0200

    the name of the claim was changed so I updated the mehtod GetUserId

--- HeRoBackEnd/Controllers/RecruitmentController.cs
+++ HeRoBackEnd/Controllers/RecruitmentController.cs
@@ -26,7 +26,7 @@ namespace HeRoBackEnd.Controllers
         private int GetUserId()
         {
             List<Claim> claims = HttpContext.User.Claims.ToList();
-            Claim idClaim = claims.FirstOrDefault(e => e.Type == ClaimTypes.NameIdentifier);
+            Claim idClaim = claims.FirstOrDefault(e => e.Type == "Id");
             int id = int.Parse(idClaim.Value);
 
             return id;
