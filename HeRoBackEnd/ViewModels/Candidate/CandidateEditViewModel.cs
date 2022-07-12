1d0d1f50560dc7a2a8221a3167826f578add1e24
Mateusz Jeżewski <mateusz.jezewski@softwarehut.com>
2022-07-12 12:34:40 +0200

    fix: typo

--- HeRoBackEnd/ViewModels/Candidate/CandidateEditViewModel.cs
+++ HeRoBackEnd/ViewModels/Candidate/CandidateEditViewModel.cs
@@ -4,35 +4,35 @@ namespace HeRoBackEnd.ViewModels.Candidate
 {
     public class CandidateEditViewModel
     {
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string LastName { get; set; }
 
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string PhoneNumber { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public DateTime? AvailableFrom { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public int? ExpectedMonthlySalary { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string? OtherExpectations { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string CvPath { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public int RecruitmentId { get; set; }
 
-        [Required(ErrorMessage = "This filed is required")]
+        [Required(ErrorMessage = "This field is required")]
         public string Status { get; set; }
     }
 }
