041126671a86bf28a313df89f82386aca94bb7a5
Eryczeslaw <erykh77@gmail.com>
2022-07-20 09:13:40 +0200

    remove mockups

--- Tests/InterviewTests/BaseTest.cs
+++ Tests/InterviewTests/BaseTest.cs
@@ -1,4 +1,4 @@
-﻿using Data.RepositoriesMockup;
+﻿using Data.IRepositories;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Services.Services;
@@ -8,12 +8,12 @@ namespace Tests.InterviewTests
     public class BaseTest
     {
         public InterviewService InterviewService { get; set; }
-        public Mock<InterviewRepositoryMockup> InterviewRepository { get; set; }
+        public Mock<IInterviewRepository> InterviewRepository { get; set; }
         public ILogger<InterviewService> Logger { get; set; }
 
         public BaseTest()
         {
-            InterviewRepository = new Mock<InterviewRepositoryMockup>();
+            InterviewRepository = new Mock<IInterviewRepository>();
             InterviewService = new InterviewService(Logger, InterviewRepository.Object);
         }
     }
