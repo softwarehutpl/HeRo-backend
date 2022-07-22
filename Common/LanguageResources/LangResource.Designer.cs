a1c5edd9f05089d4bf8fe499d40e7d9e06a9f92b
Kprzyby <kprzy@vp.pl>
2022-07-22 10:21:35 +0200

    changed file names

--- Common/LanguageResources/LangResource.Designer.cs
+++ Common/LanguageResources/LangResource.Designer.cs
@@ -22,14 +22,14 @@ namespace Common.LanguageResources {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class GermanResource {
+    internal class LangResource {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal GermanResource() {
+        internal LangResource() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Common.LanguageResources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Common.LanguageResources.GermanResource", typeof(GermanResource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Common.LanguageResources.LangResource", typeof(LangResource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
